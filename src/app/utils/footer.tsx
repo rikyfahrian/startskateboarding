e578b556bd4be4c92d509d5d464385319b5d7c99
rikyfahrian <rikyfhrian@gmail.com>
2026-05-20 14:36:37 +0700

    ui: add footer

---
+++ src/app/utils/footer.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+export function Footer() {
+  return (
+    <footer className="w-full bg-background/50 backdrop-blur-sm">
+      <div className="container mx-auto py-4 text-center text-sm text-muted-foreground">
+        Made with love from Skateboarder to Skateboarder.
+      </div>
+    </footer>
+  );
+}
