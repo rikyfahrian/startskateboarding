aa61ad99c705d40fa964fcc460c5fb6d9ce50f3d
rikyfahrian <rikyfhrian@gmail.com>
2026-06-12 14:35:53 +0700

    initial deliver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import Image from "next/image";
 import SkateboardDeckFinder from "./feature/size";
 
 export default function HomePage() {
@@ -8,6 +9,26 @@ export default function HomePage() {
       <h1 className="text-3xl font-medium tracking-tight self-center">
         Panduan Merakit Skateboard
       </h1>
+
+      <div className="relative group self-center cursor-pointer">
+        <Image
+          src="/deckgriptape2.png"
+          alt="Tekstur Griptape"
+          width={500}
+          height={400}
+          className="rounded-lg object-cover transition-opacity duration-300 group-hover:opacity-0"
+        />
+        <Image
+          src="/deckgriptape.png"
+          alt="Deck Hover"
+          width={500}
+          height={400}
+          className="absolute inset-0 rounded-lg object-cover transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+        />
+        <p className="text-xs text-muted-foreground text-center">
+          Skateboard lengkap dengan griptape yang sudah dipasang
+        </p>
+      </div>
 
       <SkateboardDeckFinder />
     </div>
